9908b9f434899ba26768606034ffca5197315a3e
Phil Cote <cotejrp@gmail.com>
2017-02-14 22:05:12 -0500

    Model code implementation for getting a certain number of tweets for a given twitter handle

--- roles/twitterapp/files/model.py
+++ roles/twitterapp/files/model.py
@@ -6,6 +6,7 @@ import os
 import hashlib
 import re
 import string
+import twitter
 
 
 db_name = "appname"
@@ -22,9 +23,19 @@ db_name = config_sec.get("database")
 db_url = "mysql+pymysql://{}:{}@localhost/{}".format(user, pw, db_name)
 eng = create_engine(db_url)
 
+# TWITTER API SETUP
+oauth_sec = cp["oauth"]
+consumer_key = oauth_sec.get("consumer_key")
+consumer_secret = oauth_sec.get("consumer_secret")
+access_token_key = oauth_sec.get("access_token_key")
+access_token_secret = oauth_sec.get("access_token_secret")
+api = twitter.Api(consumer_key=consumer_key,
+                  consumer_secret=consumer_secret,
+                  access_token_key=access_token_key,
+                  access_token_secret=access_token_secret)
+
 
 Base = declarative_base()
-
 
 class User(Base):
     __tablename__ = "users"
@@ -105,3 +116,16 @@ def set_authenticate(uname, auth_status):
     sess.add(user)
     sess.commit()
     sess.close()
+
+
+def get_tweets(screen_name, num_tweets):
+    timeline = api.GetUserTimeline(screen_name=screen_name, count=num_tweets)
+    timeline = [tweet.AsDict() for tweet in timeline]
+    cleaned_up_list = []
+    for entry in timeline:
+        short_entry = dict()
+        short_entry["screen_name"] = entry["user"]["screen_name"]
+        short_entry["message"] = entry["text"]
+        short_entry["date"] = entry["created_at"]
+        cleaned_up_list.append(short_entry)
+    return cleaned_up_list
